104a10b4ba728f3dffe0d2b9ac30c1905b2fa786
CheongYa <chch1234569@naver.com>
2022-12-09 17:34:32 +0900

    feat(spring): JPA

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional // jap를 사용하기 위해서 항상 있어야함. 서비스 계층에서 데이터를 저장하거나 변경할 때 필요.
 public class MemberService {
 
     private final MemberRepository memberRepository;
